0e7c79c49a71f81d717830792fd0ab40d3bd7e57
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-11-16 19:25:12 -0300

    Métodos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -8,17 +8,40 @@
             <li>PHP</li>
             <li>Python</li>
         </ul>
-        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <div>
+            <button @click="showEmail">{{ texto_botao }}</button>
+        </div>
+        <p v-show="mostrar_email">Mande mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio basta clicar <a v-bind:href="meu_link" target="_blank">aqui</a></p>
+        <Picture />
     </div>
 </template>
 <script>
+
+    import Picture from './Picture.vue';
+
     export default {
         name: 'Info',
+        components: {
+            Picture
+        },
         data() {
             return {
                 esta_trabalhando: false,
                 mostrar_email: false,
-                email: 'gustavoalvesdasilva@outlook.com'
+                email: 'gustavoalvesdasilva@outlook.com',
+                meu_link: 'https://gustavoalvesdev.com.br',
+                texto_botao: 'Mostrar e-mail'
+            }
+        },
+        methods: {
+            showEmail() {
+                this.mostrar_email = !this.mostrar_email
+                if (!this.mostrar_email) {
+                    this.texto_botao = "Mostrar e-mail"
+                } else {
+                    this.texto_botao = "Esconder e-mail"
+                }
             }
         }
     }
--- 2_projeto_vue/src/components/LifeCycle.vue
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -15,11 +15,20 @@
             setTimeout(() => {
                 this.nome = 'Gustavo'
             }, 1000)
+
+            this.lifeCycle()
         },
         mounted() {
             setTimeout(() => {
                 this.nome = 'Pedro'
             }, 2000)
+
+            this.lifeCycle()
+        },
+        methods: {
+            lifeCycle() {
+                console.log("Executou");
+            }
         }
     }
 
