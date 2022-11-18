07e5e325da4a1dde36ac4de47977c7254eee4bd5
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-11-18 18:54:00 -0300

    Múltiplos eventos

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="primeiro($event), segundo($event)">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>    
+</template>
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return {
+            multiplos_eventos: ""
+        }
+    },
+    methods: {
+        primeiro(e) {
+            e.preventDefault();
+            this.multiplos_eventos = "Primeiro evento ativado";
+        },
+        segundo(e) {
+            e.preventDefault();
+
+            setTimeout(() => {
+                this.multiplos_eventos = "Segundo evento!"
+            }, 500)
+        }
+    }
+}   
+</script>
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
